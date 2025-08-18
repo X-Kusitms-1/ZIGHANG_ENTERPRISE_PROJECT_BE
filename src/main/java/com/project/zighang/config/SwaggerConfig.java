592d5a07a37abebe2fbae861838d0b5a230f1924
Joon9750 <junhyeok2166@daum.net>
2025-08-18 16:01:39 +0900

    #16 ADD: SwaggerConfig 클래스 구현

---
+++ src/main/java/com/project/zighang/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.project.zighang.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${swagger.local.server.url}")
+    private String localServerUrl;
+
+    @Value("${swagger.staging.server.url}")
+    private String stagingServerUrl;
+
+    @Value("${swagger.production.server.url}")
+    private String productionServerUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Zighang API Document")
+                .version("v1.0.0")
+                .description("직행 프로젝트의 API 명세서입니다.");
+
+        Server localServer = new Server().url(localServerUrl).description("Local server");
+        Server stgServer = new Server().url(stagingServerUrl).description("Staging server");
+        Server prdServer = new Server().url(productionServerUrl).description("Production server");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info)
+                .servers(Arrays.asList(localServer, stgServer, prdServer));
+    }
+}
