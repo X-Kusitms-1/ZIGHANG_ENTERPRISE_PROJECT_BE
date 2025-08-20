96b39ef87ff098b930f885be8791e559918cca9e
Joon9750 <junhyeok2166@daum.net>
2025-08-20 18:20:31 +0900

    #24 Add: global/exception 폴더 추가 및 Error 열거형 구현

--- src/main/java/com/project/zighang/global/config/SwaggerConfig.java
+++ src/main/java/com/project/zighang/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.project.zighang.config;
+package com.project.zighang.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
---
+++ src/main/java/com/project/zighang/global/exception/Success.java
@@ -0,0 +1,10 @@
+package com.project.zighang.global.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum Success {
+}
