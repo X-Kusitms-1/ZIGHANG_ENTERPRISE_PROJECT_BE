d7600f83f2a51b7760b519260d955d06075fd955
Joon9750 <junhyeok2166@daum.net>
2025-08-27 20:41:09 +0900

    #31 CHORE: Swagger description 수정

--- src/main/java/com/project/zighang/oauth2/controller/KakaoLoginController.java
+++ src/main/java/com/project/zighang/oauth2/controller/KakaoLoginController.java
@@ -22,7 +22,8 @@ public class KakaoLoginController {
     @Operation(
             summary = "카카오 소셜 로그인",
             description = """
-    카카오 인가 코드로 소셜 로그인을 진행하며, 신규 유저일 경우 자동 가입됩니다. 성공 시 서비스 자체 JWT(Access/Refresh Token)와 신규 가입 여부를 반환합니다.
+    카카오 인가 코드로 소셜 로그인을 진행하며, 성공 시 서비스 자체 JWT(Access/Refresh Token)와 신규 가입 여부를 반환합니다.
+    처음 로그인한 유저의 경우 isNewUser가 참으로 내려갑니다.
     """
     )
     public ResponseEntity<TokenResult> kakaoLogin(
