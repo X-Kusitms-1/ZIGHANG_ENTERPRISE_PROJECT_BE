6991627ce796756b9dfb3d54d43ac67cfc1e0789
Joon9750 <junhyeok2166@daum.net>
2025-08-27 17:02:45 +0900

    #31 CHORE: TokenResult class 수정

--- src/main/java/com/project/zighang/oauth2/dto/TokenResult.java
+++ src/main/java/com/project/zighang/oauth2/dto/TokenResult.java
@@ -1,7 +1,5 @@
 package com.project.zighang.oauth2.dto;
 
-import com.project.zighang.user.entity.UserEntity;
-
 public record TokenResult(TokenDto tokenDto, Boolean isNewUser) {
     public static TokenResult from(final TokenDto tokenDto, boolean isNewUser){
         return new TokenResult(tokenDto, isNewUser);
