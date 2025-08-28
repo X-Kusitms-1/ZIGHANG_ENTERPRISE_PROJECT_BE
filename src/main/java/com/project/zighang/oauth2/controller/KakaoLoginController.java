6fd926ce48a177aaaebae3d2b0324ab23e18c40d
Joon9750 <junhyeok2166@daum.net>
2025-08-28 23:59:25 +0900

    #31 FIX: API 응답 ResponseEntity에서 RspTemplate으로 수정

--- src/main/java/com/project/zighang/oauth2/controller/KakaoLoginController.java
+++ src/main/java/com/project/zighang/oauth2/controller/KakaoLoginController.java
@@ -1,11 +1,12 @@
 package com.project.zighang.oauth2.controller;
 
+import com.project.zighang.global.exception.Success;
+import com.project.zighang.global.template.RspTemplate;
 import com.project.zighang.oauth2.dto.TokenResult;
 import com.project.zighang.oauth2.service.KakaoLoginService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,9 +27,10 @@ public class KakaoLoginController {
     처음 로그인한 유저의 경우 isNewUser가 참으로 내려갑니다.
     """
     )
-    public ResponseEntity<TokenResult> kakaoLogin(
-            @NotBlank @RequestParam("code") String code) {
+    public RspTemplate<?> kakaoLogin(
+            @NotBlank @RequestParam("code") String code
+    ) {
         TokenResult result = kakaoLoginService.login(code);
-        return ResponseEntity.ok(result);
+        return RspTemplate.success(Success.CREATE_JWT_TOKEN_SUCCESS, result);
     }
 }
