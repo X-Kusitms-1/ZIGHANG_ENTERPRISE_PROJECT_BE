e80da3309420ba09c6529e4cda75b2ca49db2fff
Joon <83629193+Joon9750@users.noreply.github.com>
2025-09-06 13:18:05 +0900

    Update SecurityConfig.java

--- src/main/java/com/project/zighang/global/config/SecurityConfig.java
+++ src/main/java/com/project/zighang/global/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
+                                "https://zighang.vercel.app/",
                                 "/api/companies/**",
                                 "/**",
                                 "/v1/auth/**",
