1fda6010bf5b2c5bcb10bd6c360d443d7a9b890b
seongho5356 <kjeng7897@gmail.com>
2025-09-09 14:08:37 +0900

    #66 Refactor: userId 대신 @AuthenticationPrincial을 사용한다

--- src/main/java/com/project/zighang/domain/subscription/controller/SubscriptionController.java
+++ src/main/java/com/project/zighang/domain/subscription/controller/SubscriptionController.java
@@ -6,8 +6,11 @@ import com.project.zighang.domain.subscription.entity.UserCompanySubscription;
 import com.project.zighang.domain.subscription.service.SubscriptionService;
 import com.project.zighang.global.exception.Success;
 import com.project.zighang.global.template.RspTemplate;
+import com.project.zighang.user.entity.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +23,9 @@ public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
     @PostMapping
-    public RspTemplate<List<SubscriptionResponse>> register(@Valid @RequestBody SubscriptionRequest request) {
-        List<UserCompanySubscription> subscriptions = subscriptionService.register(request.userId(), request.companyId());
+    public RspTemplate<List<SubscriptionResponse>> register(@RequestParam Long companyId,
+                                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<UserCompanySubscription> subscriptions = subscriptionService.register(userDetails.getId(), companyId);
 
         List<SubscriptionResponse> subscriptionResponse = subscriptions.stream()
                 .map(sub -> new SubscriptionResponse(sub.getId(), sub.getUserId(), sub.getCompanyId()))
@@ -31,8 +35,9 @@ public class SubscriptionController {
     }
 
     @DeleteMapping
-    public RspTemplate<List<SubscriptionResponse>> unregister(@Valid @RequestBody SubscriptionRequest request) {
-        List<UserCompanySubscription> subscriptions = subscriptionService.unregister(request.userId(), request.companyId());
+    public RspTemplate<List<SubscriptionResponse>> unregister(@RequestParam Long companyId,
+                                                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<UserCompanySubscription> subscriptions = subscriptionService.unregister(userDetails.getId(), companyId);
 
         List<SubscriptionResponse> subscriptionResponse = subscriptions.stream()
                 .map(sub -> new SubscriptionResponse(sub.getId(), sub.getUserId(), sub.getCompanyId()))
