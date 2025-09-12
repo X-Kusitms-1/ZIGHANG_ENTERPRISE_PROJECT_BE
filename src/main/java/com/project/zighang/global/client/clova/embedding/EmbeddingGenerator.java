66a9580d670da735c256c775801d59078b748507
seongho5356 <kjeng7897@gmail.com>
2025-09-13 00:28:23 +0900

    #64 Chore: 주석을 작성한다

--- src/main/java/com/project/zighang/global/client/clova/embedding/EmbeddingGenerator.java
+++ src/main/java/com/project/zighang/global/client/clova/embedding/EmbeddingGenerator.java
@@ -3,5 +3,11 @@ package com.project.zighang.global.client.clova.embedding;
 import java.util.List;
 
 public interface EmbeddingGenerator {
+    /**
+     * 주어진 텍스트를 임베딩 벡터로 변환합니다.
+     *
+     * @param text 변환할 텍스트
+     * @return 임베딩 벡터 (List<Double> 형태)
+     */
     List<Double> embed(String text);
 }
